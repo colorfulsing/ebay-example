ada7f2c91a5a1bf00d587305d58b0a80b18ae671
Parama Danoesubroto <parama@answersengine.com>
2019-02-04 13:25:53 -0500

    added save outputs on a seeder

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,3 +1,22 @@
+
+outputs << {
+    _collection: 'seeds',
+    _id:"iphone",
+    search: "iphone"
+}
+
+save_outputs(outputs)
+puts "saved_output"
+
+puts "finding output #{find_output("seeds", {_id: "iphone"})}"
+
+outputs << {
+    _gid: "www.ebay.com-f06bd9f00f29e937ef9176ff97daf24a",
+    _collection: 'seeds',
+    search: "iphone",
+    dependent_record: find_output("seeds", {_id: "iphone"}),
+}
+
 pages << {
     page_type: 'listings', 
     method: "GET",
