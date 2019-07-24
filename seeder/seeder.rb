ffbac2a925d2d0dc1ed7a80bfb15b7eb4fc332a3
Eduardo Rosales <eduardo@datahen.com>
2019-07-24 09:47:31 -0600

    Use headless browser.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,5 +2,6 @@ pages << {
     page_type: 'listings',
     method: "GET",
     url: "https://www.ebay.com/b/Apple-iPhone/9355/bn_319682",
-    freshness: Time.now.utc.strftime('%FT%TZ')
+    freshness: Time.now.utc.strftime('%FT%TZ'),
+    fetch_type: 'browser'
 }
